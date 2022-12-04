60e2c2a907d851bfaf84b35816ddca5477a2df67
Josh <me@joshl.io>
2022-12-04 03:48:26 +0000

    added default files

---
+++ modules/default-token.py
@@ -0,0 +1,8 @@
+# this file contains the bot's token, which is used to log in to discord
+# to use your own bot's token, please do the following:
+
+# 1. create a file in the 'modules' folder called token.py
+# 2. copy the following code into token.py:
+# 3. replace the text in quotes with your bot's token
+
+value = "xxxxxxxxxxxxxxxxxxxxxxxxx"
